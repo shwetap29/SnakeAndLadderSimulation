5ea6501259d7397b28b6bbdb9e7326597c415d4e
shwetap29 <shwetaprasad48@gmail.com>
2021-05-21 23:37:58 +0530

    CONDITION:3 player condition added

--- src/main/java/com/bridgelabz/SnakeAndLadderSimulation.java
+++ src/main/java/com/bridgelabz/SnakeAndLadderSimulation.java
@@ -1,11 +1,33 @@
 package com.bridgelabz;
 
 public class SnakeAndLadderSimulation {
+    public static final int NO_PLAY = 0;
+    public static final int LADDER = 1;
+    public static final int SNAKE = 2;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder game.");
 
-           int startPosition=0;
+           int Position=0;
+           int newPosition=0;
+
            int DieRolled = (int) (Math.random()*6)+1;
-           System.out.println(DieRolled);
+           int checkOption = (int) (Math.random()*3);
+
+           switch (checkOption){
+               case NO_PLAY:
+                     newPosition = 0;
+                      break;
+               case LADDER:
+                   newPosition = DieRolled;
+                     break;
+               case SNAKE:
+                   newPosition = -DieRolled;
+           }
+           Position=Position+newPosition;
+           if (Position<0){
+                    Position = 0;
+           }
+           System.out.println("You are now at "+Position+"th"+" position");
     }
 }
