b546ebc98bf32cfad9d3e33094bd0da88a3e880f
shwetap29 <shwetaprasad48@gmail.com>
2021-05-22 08:04:22 +0530

    ADDED: Win position

--- src/main/java/com/bridgelabz/SnakeAndLadderSimulation.java
+++ src/main/java/com/bridgelabz/SnakeAndLadderSimulation.java
@@ -8,26 +8,29 @@ public class SnakeAndLadderSimulation {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder game.");
 
-           int Position=0;
-           int newPosition=0;
+        int Position = 0;
+        int newPosition = 0;
+        int WinPosition = 100;
 
-           int DieRolled = (int) (Math.random()*6)+1;
-           int checkOption = (int) (Math.random()*3);
+        while (Position <= WinPosition) {
+            int DieRolled = (int) (Math.random() * 6) + 1;
+            int checkOption = (int) (Math.random() * 3);
 
-           switch (checkOption){
-               case NO_PLAY:
-                     newPosition = 0;
-                      break;
-               case LADDER:
-                   newPosition = DieRolled;
-                     break;
-               case SNAKE:
-                   newPosition = -DieRolled;
-           }
-           Position=Position+newPosition;
-           if (Position<0){
-                    Position = 0;
-           }
-           System.out.println("You are now at "+Position+"th"+" position");
+            switch (checkOption) {
+                case NO_PLAY:
+                    newPosition = 0;
+                    break;
+                case LADDER:
+                    newPosition = DieRolled;
+                    break;
+                case SNAKE:
+                    newPosition = -DieRolled;
+            }
+            Position = Position + newPosition;
+            if (Position < 0) {
+                Position = 0;
+            }
+            System.out.println("You are now at " + Position + "th" + " position");
+        }
     }
 }
